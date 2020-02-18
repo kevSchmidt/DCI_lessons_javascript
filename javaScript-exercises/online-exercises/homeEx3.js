fd7415ac7c2ce771c741ea45e3f976e46eadcd4e
Kevin <kev.saintigny.schmidt@gmail.com>
2020-02-18 18:57:01 +0100

    updated

--- javaScript-exercises/online-exercises/homeEx3.js
+++ javaScript-exercises/online-exercises/homeEx3.js
@@ -1,31 +1,35 @@
-// Given what is supposed to be typed and what is actually typed, write a function that returns the broken key(s). The function looks like:
-// findBrokenKeys(correct phrase, what you actually typed)
-// Notes
-// Broken keys should be ordered by when they first appear in the sentence.
-// Only one broken key per letter should be listed.
-// Letters will all be in lower case.
-// Examples
+// - Given what is supposed to be typed and what is actually typed, write a function that returns the broken key(s).
+// The function looks like: findBrokenKeys(correct phrase, what you actually typed)
+// **Notes**
+// 1. Broken keys should be ordered by when they first appear in the sentence.
+// 2. Only one broken key per letter should be listed.
+// 3. Letters will all be in lower case.
+// **Examples**
 // findBrokenKeys("happy birthday", "hawwy birthday") ➞ ["p"]
 // findBrokenKeys("starry night", "starrq light") ➞ ["y", "n"]
 // findBrokenKeys("beethoven", "affthoif5") ➞ ["b", "e", "v", "n"]
+
 const findBrokenKeys = (correctArg, arg) => {
+  // we want the result in an array
   let result = [];
+
+  // we loop inside the length of the correct phrase
   for (let i = 0; i < correctArg.length; i++) {
+    // we compare letter by letter the correct phrase and what is actually typed
     if (arg[i] !== correctArg[i]) {
+      // we check if a letter is already in our result array
       if (!result.includes(correctArg[i])) {
+        // we push this letter if not already present
         result.push(correctArg[i]);
       }
     }
   }
   return result;
 };
+
 console.log(findBrokenKeys("happy birthday", "hawwy birthday"));
 console.log(findBrokenKeys("starry night", "starrq light"));
 console.log(findBrokenKeys("beethoven", "affthoif5"));
-
-// function findBrokenKeys(str1, str2) {
-//   return [...new Set(str1.split("").filter((a, index) => a !== str2[index]))];
-// }
 
 // Given an object of how many more pages each ink color can print, output the maximum number of pages the printer can print before any of the colors run out.
 // Examples
@@ -90,25 +94,21 @@ console.log(doubleChar("1234!_ "));
 // [2, 7, 6, 9, 5, 1, 4, 3, 8] -> true
 // [3, 5, 7, 8, 1, 6, 4, 9, 2] -> false
 // C -> false
-console.log("-> magicSquare :");
+console.log("magicSquare:");
 const magicSquare = arr => {
-  let row1 = arr[0] + arr[1] + arr[2];
-  let row2 = arr[3] + arr[4] + arr[5];
-  let row3 = arr[6] + arr[7] + arr[8];
-  let row4 = arr[0] + arr[3] + arr[6];
-  let row5 = arr[1] + arr[4] + arr[7];
-  let row6 = arr[2] + arr[5] + arr[8];
-  let diag1 = arr[0] + arr[4] + arr[8];
-  let diag2 = arr[2] + arr[4] + arr[6];
   return (
     arr.length === 9 &&
-    row1 === 15 &&
-    row2 === 15 &&
-    row3 === 15 &&
-    row4 === 15 &&
-    row5 === 15 &&
-    row6 === 15 &&
-    diag1 === 15 &&
-    diag2 === 15
+    arr[0] + arr[1] + arr[2] === 15 &&
+    arr[3] + arr[4] + arr[5] === 15 &&
+    arr[6] + arr[7] + arr[8] === 15 &&
+    arr[0] + arr[3] + arr[6] === 15 &&
+    arr[1] + arr[4] + arr[7] === 15 &&
+    arr[2] + arr[5] + arr[8] === 15 &&
+    arr[0] + arr[4] + arr[8] === 15 &&
+    arr[2] + arr[4] + arr[6] === 15
   );
 };
+console.log(magicSquare([8, 1, 6, 3, 5, 7, 4, 9, 2]));
+console.log(magicSquare([2, 7, 6, 9, 5, 1, 4, 3, 8]));
+console.log(magicSquare([3, 5, 7, 8, 1, 6, 4, 9, 2]));
+console.log(magicSquare("c"));
