6da82848fb70fab0c29300b99c1b4b8b973bc3ca
Kevin <kev.saintigny.schmidt@gmail.com>
2020-02-23 17:44:18 +0100

    more exercises

--- js-exercises/online-exercises/schoolEx.js
+++ js-exercises/online-exercises/schoolEx.js
@@ -74,7 +74,8 @@ function calorie(age, amount) {
 }
 console.log(calorie(25, 2));
 
-// Create a function that determines whether or not it’s possible to split a pie fairly given these three parameters:
+// **Equal Slices**
+// - Create a function that determines whether or not it’s possible to split a pie fairly given these three parameters:
 // Total number of slices. Number of recipients. How many slices each person gets.
 // **Examples**
 // equalSlices(11, 5, 3) ➞ false // 5 people x 3 slices each = 15 slices > 11 slices
