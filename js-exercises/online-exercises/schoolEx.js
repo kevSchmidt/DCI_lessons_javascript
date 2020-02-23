e833681488f54717863e0d4255dd1e2b6349dad0
Kevin <kev.saintigny.schmidt@gmail.com>
2020-02-23 17:45:57 +0100

    more exercises

--- js-exercises/online-exercises/schoolEx.js
+++ js-exercises/online-exercises/schoolEx.js
@@ -32,8 +32,8 @@ console.log(twofer()); // -> "Two for me and one for you"
 
 // Complete the function below so that it still calculates the power of a number even if the `exp` argument is not passed to it.
 // The default `exp` should be set to 2.
-// **Example**
-// Calling the function with 2 and 4 will return 16 (2 to the power of 4), If you call it with just 2, it should return 4 (2 to the power of 2).
+// Example: calling the function with 2 and 4 will return 16 (2 to the power of 4),
+// if you call it with just 2, it should return 4 (2 to the power of 2).
 const expo = (num, exp = 2) => {
   let result = 1;
   for (let i = 0; i < exp; i++) {
@@ -57,6 +57,7 @@ console.log(userAge(1993));
 // **Count Occurrences**
 // - Create a function that accepts two arguments: a string and a letter.
 // The function should count the number of occurrences of that letter in the string.
+// **Example**
 // countOccurrences(“this is a string”, “i”) ➞ 3
 function occurrences(string, letter) {
   let str = string.split(letter).length - 1;
