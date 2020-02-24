29e63be1e725d2c600381a1161c65a2ddcfb7e1e
Kevin <kev.saintigny.schmidt@gmail.com>
2020-02-24 14:23:27 +0100

    added exercises

---
+++ exercises/online-exercises/homeEx.js
@@ -0,0 +1,115 @@
+// Given an object of people and their ages, return how old the people would be after n years have passed. Use the absolute value of n.
+// Assume that everyone is immortal (it would be a bit grim if I told you to remove names once they reached 75).
+// n should be a positive number because last time I checked, people don't tend to age backwards. Therefore, use the absolute value of n.
+console.log("------After New Years------");
+const afterNYears = (list, n) => {
+  for (let age in list) list[age] += Math.abs(n); // Hadi -> list      30 -> list[age]
+  return list;
+};
+console.log(
+  afterNYears(
+    {
+      Hadi: 30,
+      Ioannes: 37,
+      Simona: 28,
+      Mauricio: 32,
+      Baby: 1
+    },
+    1 // 1 -> n
+  )
+);
+
+// A set is a collection of unique items. A set can be formed from an array from removing all duplicate items.
+// Create a function that transforms an array into a set.
+// **Examples**
+// set([1, 3, 3, 5, 5]) ➞ [1, 3, 5]
+// set([4, 4, 4, 4]) ➞ [4]
+console.log("------set------");
+function set(array) {
+  return array.filter((n, index) => array.indexOf(n) == index);
+}
+console.log(set([1, 3, 3, 5, 5]));
+console.log(set([4, 4, 4, 4]));
+
+// **FindLargestNums**
+// - Create a function that takes an array of arrays with numbers. Return a new (single) array with the largest numbers of each.
+// **Example**
+// findLargestNums([[4, 2, 7, 1], [20, 70, 40, 90], [1, 2, 0]]) ➞ [7, 90, 2]
+// findLargestNums([[-34, -54, -74], [-32, -2, -65], [-54, 7, -43]]) ➞ [-34, -2, 7]
+console.log("------findLargestNums------:");
+function findLargestNums(arr) {
+  return arr.map(x => Math.max(...x));
+}
+console.log(
+  findLargestNums([
+    [4, 2, 7, 1],
+    [20, 70, 40, 90],
+    [1, 2, 0]
+  ])
+);
+console.log(
+  findLargestNums([
+    [-34, -54, -74],
+    [-32, -2, -65],
+    [-54, 7, -43]
+  ])
+);
+
+// **Array filter**
+// - Create a function that takes an array of non-negative numbers and strings and return a new array without the strings.
+// **Examples**
+// filterArray([1, 2, "a", "b"]) ➞ [1, 2]
+// filterArray([1, "a", "b", 0, 15]) ➞ [1, 0, 15]
+// filterArray([1, 2, "aasf", "1", "123", 123]) ➞ [1, 2, 123]
+console.log("------filterArray------");
+function filterArray(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "number") {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
+console.log(filterArray([1, 2, "a", "b"]));
+console.log(filterArray([1, "a", "b", 0, 15]));
+console.log(filterArray([1, 2, "aasf", "1", "123", 123]));
+
+// **Society Name**
+// A group of friends have decided to start a secret society. The name will be the first letter of each of their names, sorted in alphabetical order.
+// - Create a function that takes in an array of names and returns the name of the secret society:
+// **Examples**
+// societyName(["Adam", "Sarah", "Malcolm"]) ➞ "AMS"
+// societyName(["Harry", "Newt", "Luna", "Cho"]) ➞ "CHLN
+console.log("------societyName------");
+function societyName(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    let firstLetter = arr[i].substring(0, 1);
+    result.push(firstLetter);
+  }
+  return result.sort().join(""); // sort() give us the alphabet order
+}
+console.log(societyName(["Adam", "Sarah", "Malcolm"]));
+console.log(societyName(["Harry", "Newt", "Luna", "Cho"]));
+
+// Zip codes consist of 5 consecutive digits. Given a string, write a function to determine whether the input is a valid zip code. A valid zip code is as follows:
+// Must only contain numbers (no non-digits allowed).
+// Must not contain any spaces.
+// Must not be greater than 5 digits in length.
+// isValid("59001") ➞ true
+// isValid("853a7") ➞ false
+// isValid("732 32") ➞ false
+// isValid("393939") ➞ false
+console.log("------isValid------");
+function isValid(arr) {
+  if (arr.length == 5 && !isNaN(arr)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isValid("59001"));
+console.log(isValid("853a7"));
+console.log(isValid("732 32"));
+console.log(isValid("393939"));
---
+++ exercises/online-exercises/homeEx2.js
@@ -0,0 +1,124 @@
+// **Ping Pong**
+// A game of table tennis almost always sounds like Ping! followed by Pong!
+// Therefore, you know that Player 2 has won if you hear Pong!as the last sound
+// (since Player 1 didn't return the ball back).
+// Given an array of Ping!, create a function that inserts Pong! in between each element. Also:
+// If win equals true, end the list with Pong!. If win equals false, end with Ping! instead.
+// pingPong(["Ping!"], true) ➞ ["Ping!", "Pong!"]
+// pingPong(["Ping!", "Ping!"], false) ➞ ["Ping!", "Pong!", "Ping!"]
+// pingPong(["Ping!", "Ping!", "Ping!"], true) ➞ ["Ping!", "Pong!", "Ping!", "Pong!", "Ping!", "Pong!"]
+// You will always return the ball (i.e. the Pongs are yours). Player 1 serves the ball and makes Ping!.
+// Return an array of strings.
+console.log("------Ping Pong------");
+const pingPong = (arr, win) => {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    result.push(arr[i]) + result.push(" Pong!");
+  }
+  if (win) {
+    return result;
+  } else {
+    result.pop();
+    return result;
+  }
+};
+// const pingPong = (arr, win) => {
+//   let result = [];
+//   arr.forEach(element => {
+//     result.push(element) + result.push("Pong");
+//   });
+//   return win ? result : result.slice(0, result.length - 1);
+// };
+console.log(pingPong(["Ping!"], true));
+console.log(pingPong(["Ping!", "Ping!"], false));
+console.log(pingPong(["Ping!", "Ping!", "Ping!"], false));
+
+// **Reverse**
+// - Create a function that reverses a string. Throw a custom error if the user inputs another type as an argument.
+// **Example**
+// reverseString(2019) -> ERROR! This is not a string!
+console.log("------reverseString------");
+function reverseString(str) {
+  try {
+    if (typeof str !== "string") {
+      throw `ERROR! This is not a string!`;
+    } else {
+      newStr = str
+        .split("")
+        .reverse()
+        .join("");
+      console.log(newStr);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+reverseString(2019);
+reverseString("Hello");
+
+// **Century**
+// - Create a function that takes in a year and returns the correct century.
+// **Examples**
+// century(1756) ➞ "18th century"
+// century(1555) ➞ "16th century"
+// century(1000) ➞ "10th century"
+// century(1001) ➞ "11th century"
+// century(2005) ➞ "21st century"
+console.log("------Century------");
+function century(num) {
+  year = num.toString();
+  if (year.endsWith("00")) {
+    console.log(`${Math.floor(num / 100)}th century`);
+  } else if (num < 2000) {
+    console.log(`${Math.floor(num / 100 + 1)}th century`);
+  } else {
+    console.log(`${Math.floor(num / 100 + 1)}st century`);
+  }
+}
+century(1756);
+century(1555);
+century(1000);
+century(1001);
+century(2005);
+
+// **Match Houses**
+// - Create a function that takes a number (step) as an argument and returns the amount of matchsticks in that step.
+// See step 1, 2 and 3 in the image above.
+// **Examples**
+// matchHouses(1) ➞ 6
+// matchHouses(4) ➞ 21
+// matchHouses(87) ➞ 436
+// **Notes**
+// Step 0 returns 0 matchsticks.
+// The input (step) will always be a non-negative integer.
+console.log("------matchHouse------");
+function matchHouses(num) {
+  if (num === 1) {
+    return 6;
+  } else {
+    return num * 5 + 1;
+  }
+}
+console.log(matchHouses(1));
+console.log(matchHouses(4));
+console.log(matchHouses(87));
+
+// **Missing Number**
+// - Create a function that takes an array of all integers between 1 and 10 (excluding one) and returns the missing integer.
+// **Examples**
+// missingNums([1, 2, 3, 4, 6, 7, 8, 9, 10]) ➞ 5
+// missingNums([7, 2, 3, 6, 5, 9, 1, 4, 8]) ➞ 10
+// missingNums([10, 5, 1, 2, 4, 6, 8, 3, 9]) ➞ 7
+console.log("......missingNums......");
+function missingNums(arr) {
+  let result = 0;
+  for (let i = 1; i < 11; i++) {
+    if (!arr.includes(i)) {
+      result = i;
+    }
+  }
+  return result;
+}
+console.log(missingNums([10, 5, 1, 2, 4, 6, 8, 3, 9]));
+console.log(missingNums([1, 2, 3, 4, 6, 7, 8, 9, 10]));
+console.log(missingNums([7, 2, 3, 6, 5, 9, 1, 4, 8]));
