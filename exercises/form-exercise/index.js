1df25b0355d7b01f7006d3878fcaea71efe79c6c
Kevin <kev.saintigny.schmidt@gmail.com>
2020-04-29 16:05:45 +0200

    exercise

--- exercises/form-exercise/index.js
+++ exercises/form-exercise/index.js
@@ -1,31 +1,33 @@
-const add = () => {
-  let a = +document.querySelector("#firstNumber").value;
-  let b = +document.querySelector("#secondNumber").value;
-  let result = a + b;
+// We set our variable in the global environment
+const inputOne = document.querySelector("#firstNumber");
+const inputTwo = document.querySelector("#secondNumber");
+const inputResult = document.querySelector("#sumResult");
+const Btns = document.querySelectorAll(".myBtn");
 
-  document.querySelector("#sumResult").value = result;
-};
+Btns.forEach((elem) => {
+  // add event on click
+  elem.addEventListener("click", function (e) {
+    // we want numbers
+    let a = parseInt(inputOne.value);
+    let b = parseInt(inputTwo.value);
+    let result;
 
-const minus = () => {
-  let a = +document.querySelector("#firstNumber").value;
-  let b = +document.querySelector("#secondNumber").value;
-  let result = a - b;
+    // we select items by class name
+    switch (elem.className) {
+      case "myBtn add":
+        result = a + b;
+        break;
+      case "myBtn subtract":
+        result = a - b;
+        break;
+      case "myBtn divide":
+        result = a / b;
+        break;
+      case "myBtn multiply":
+        result = a * b;
+        break;
+    }
 
-  document.querySelector("#sumResult").value = result;
-};
-
-const multiply = () => {
-  let a = +document.querySelector("#firstNumber").value;
-  let b = +document.querySelector("#secondNumber").value;
-  let result = a * b;
-
-  document.querySelector("#sumResult").value = result;
-};
-
-const divide = () => {
-  let a = +document.querySelector("#firstNumber").value;
-  let b = +document.querySelector("#secondNumber").value;
-  let result = a % b;
-
-  document.querySelector("#sumResult").value = result;
-};
+    inputResult.value = result;
+  });
+});
