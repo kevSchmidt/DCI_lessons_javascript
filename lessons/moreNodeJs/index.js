e4d0de48f120e33c17720c0671718ff31bc15038
Kevin <kev.saintigny.schmidt@gmail.com>
2020-05-26 11:49:10 +0200

    exercise

--- lessons/moreNodeJs/index.js
+++ lessons/moreNodeJs/index.js
@@ -1,6 +1,8 @@
 const args = process.argv.slice(2);
 
-// ===== ParseNum ===
+// ========== FUNCTIONS ===
+
+// ParseNum
 function parseNum(arg) {
   const number = parseFloat(arg);
   if (isNaN(number)) {
@@ -10,20 +12,38 @@ function parseNum(arg) {
   return number;
 }
 
-// ===== Sum ===
+// Sum
 function sum(numArray) {
   return numArray.reduce((sum, n) => sum + n, 0);
 }
 
-// ===== Avg ===
+// Avg
 function avg(numArray) {
   return sum(numArray) / numArray.length;
+}
+
+// Med
+function med(numArray) {
+  // 1. Sort the set of number
+  numArray.sort((a, b) => {
+    return a - b;
+  });
+
+  // 2. Get index of the middle number
+  const midNum = numArray[(numArray.length - 1) / 2];
+
+  // 3. Decimal?
+  if (midNum === undefined) {
+    return (numArray.length - 1) / 2 + 1;
+  }
+  return midNum;
 }
 
 const [operation, ...rest] = args;
 const numbers = rest.map(parseNum);
 
-// ===== Switch ===
+// ========== SWITCH ===
+
 switch (operation) {
   case "sum":
     console.log(sum(numbers));
@@ -33,10 +53,10 @@ switch (operation) {
     console.log(avg(numbers));
     break;
 
-  case "mid":
-    console.log(mid(numbers));
+  case "med":
+    console.log(med(numbers));
     break;
 
   default:
-    console.log("Sorry, please use 'sum' or 'avg'.");
+    console.log("Please use 'sum', 'avg' or 'med' as first argument.");
 }
